1de0c1fc9add1abb331977d1077deb83b0aa2dd0
eyluloyku <eyluloyku@sabanciuniv.edu>
2023-04-05 00:46:56 +0300

    Authentication and authorization

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+import Router from "express";
+import auth from "../middleware/auth.js";
+import roleCheck from "../middleware/roleCheck.js";
+
+const router = Router();
+
+router.get("/details", auth, (req, res) => {
+	res.status(200).json({ message: "user authenticated." });
+});
+
+/*
+router.get("/details", auth, roleCheck(["sales_man"]), (req, res) => {
+	res.status(200).json({ message: "user authenticated." });
+});
+*/
+
+export default router;
