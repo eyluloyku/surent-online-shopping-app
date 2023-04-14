2869e441889e3f5fc15281ce4419d3c76ddf2470
Pelinsu Sarac <psarac@sabanciuniv.edu>
2023-04-14 23:00:15 +0300

    review for product

--- backend/models/productmodel.js
+++ backend/models/productmodel.js
@@ -3,6 +3,22 @@
 import mongoose from "mongoose"
 const schema = mongoose.Schema;
 
+const reviewSchema = mongoose.Schema(
+    {
+      name: { type: String, required: true },
+      rating: { type: Number, required: true },
+      comment: { type: String, required: true },
+      user: {
+       type: mongoose.Schema.Types.ObjectId,
+       required: true,
+       ref: 'User',
+             },
+     },
+    {
+      timestamps: true,
+    }
+  )
+
 //pass object as argument, this is our schema.
 const productSchema = new mongoose.Schema({
     Pname: {
@@ -54,7 +70,15 @@ const productSchema = new mongoose.Schema({
     images: {
         type: Array,
         default: []
+    },
+    reviews: [reviewSchema],
+     
+    numReviews: {
+        type: Number,
+        required: true,
+        default: 0,
     }
+
 },{timestamps:true})
 
 
